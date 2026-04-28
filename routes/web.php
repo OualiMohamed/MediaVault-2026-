08c312bf36b7ecad8488b3a1555e5de79b336521
OualiMohamed <mohamed.ouali2010@gmail.com>
2026-04-28 11:24:59 +0100

    Blade Entry Point

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Catch-all route to serve the SPA
+Route::view('/{any}', 'app')->where('any', '.*');
